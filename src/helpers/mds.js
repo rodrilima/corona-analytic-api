6c61ad5c3cb1ad9170e5316a8ba434bc1a921d95
raphaelkieling <raphael.kieling@egsys.com.br>
2020-03-17 16:05:28 -0300

    feat: show the history of each state

--- src/helpers/mds.js
+++ src/helpers/mds.js
@@ -1,31 +1,56 @@
 const states = require("../config/states");
 
 class mdsHelper {
-  sourceParser(body) {
-    const data = JSON.parse(body.substring(13));
-    const brazil = data["brazil"][data["brazil"].length - 1];
-    const world = data["world"][data["world"].length - 1];
-    this.brazilValuesParser.call(brazil);
-    return {
-      brazil,
-      world,
-      updated_at: Date.now()
-    };
-  }
-  brazilValuesParser() {
-    this["values"] = this["values"].map(value => {
-      return {
-        uid: value["uid"] || "",
-        state: states[value["uid"]] || "",
-        cases: value["cases"] || 0,
-        deaths: value["deaths"] || 0,
-        suspects: value["suspects"] || 0,
-        refuses: value["refuses"] || 0,
-        broadcast: value["broadcast"] || false,
-        comments: value["comments"] || ""
-      };
-    });
-  }
+    sourceParser(body) {
+        const data = JSON.parse(body.substring(13));
+        const brazil = data["brazil"];
+        const world = data["world"][data["world"].length - 1];
+
+        return {
+            brazil: this.brazilValuesParser(brazil),
+            world,
+            updated_at: Date.now()
+        };
+    }
+    brazilValuesParser(brazilList) {
+        // Create a map with all states to take the history
+        const stateMap = new Map();
+        brazilList.forEach(item => {
+            item.values.forEach(itemValue => {
+                let history = stateMap.get(itemValue.uid);
+                if (!history) stateMap.set(itemValue.uid, (history = []));
+
+                history.push({
+                    date: item.date,
+                    ...this.informationValueParser(item)
+                });
+            });
+        });
+
+        // Get the last item from brazilList array, important to not break the current users
+        const lastBrazilItem = brazilList[brazilList.length - 1];
+
+        lastBrazilItem.values = lastBrazilItem.values.map(value => {
+            return {
+                uid: value.uid || "",
+                state: states[value.uid] || "",
+                ...this.informationValueParser(value),
+                history: stateMap.get(value.uid)
+            };
+        });
+
+        return lastBrazilItem;
+    }
+    informationValueParser(information) {
+        return {
+            cases: information.cases || 0,
+            deaths: information.deaths || 0,
+            suspects: information.suspects || 0,
+            refuses: information.refuses || 0,
+            broadcast: information.broadcast || false,
+            comments: information.comments || ""
+        };
+    }
 }
 
 module.exports = new mdsHelper();
