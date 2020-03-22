22ead60fbd425c78550fdab6279b42adf2c3aa60
rodrilima <rodelima20@gmail.com>
2020-03-22 13:44:59 -0300

    fix: remove router save

--- src/routes.js
+++ src/routes.js
@@ -4,14 +4,14 @@ const coronaController = require("./app/controllers/coronaController");
 const historyController = require("./app/controllers/historyController");
 const journalController = require("./app/controllers/journalController");
 
-const mds = require("./libs/mds");
+// const mds = require("./libs/mds");
 
 routes.get("/", coronaController.index);
 
-routes.get("/save", async (req, res) => {
-  const response = await mds.save()
-  res.send(response);
-});
+// routes.get("/save", async (req, res) => {
+//   const response = await mds.save()
+//   res.send(response);
+// });
 
 routes.get("/journal", journalController.index);
 routes.get("/journal/:state", journalController.indexState);
